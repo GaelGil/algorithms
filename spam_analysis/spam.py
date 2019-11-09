f1ca1060b587da136a99e8b95d3caec737f42e09
Gael Gil <gaelgilbello27@gmail.com>
2019-11-08 22:51:29 -0800

    everything works but not acurate

--- spam_analysis/spam.py
+++ spam_analysis/spam.py
@@ -1,21 +1,22 @@
 SPAM_DICT = {} #maybe a global variable with spam likely words, idk
 NON_SPAM_DICT = {} #maybe to compare
 
-# def is_spam_or_not(message:str, message_dict:dict):
-#     """
-#     This function detemines if message is spam or 
-#     not based on ...
-#     """
-#     spam_num = 0
-#     for val in message_dict.values():
-#         spam_num += val
-#     if spam_num >= 80:
-#         add_to_spam(message)
-#         print('spam')
-#     else:
-#         print('not spam')
+def is_spam_or_not(message:list, message_dict:dict):
+    """
+    This function detemines if message is spam or 
+    not based on ...
+    """
+    spam_num = 0
+    for val in message_dict.values():
+        spam_num += val
+    if spam_num >= (len(message) * 10):
+        add_to_spam(message)
+        print('spam')
+    else:
+        print('not spam')
 
-def add_to_spam(spam:str):
+
+def add_to_spam(spam:list):
     """
     If the message is spam it'll append the words
     from the message to a dict. If words are alredy
@@ -23,15 +24,14 @@ def add_to_spam(spam:str):
     value. If the word is not in the dict it will get 
     added as a key with a value of one.
     """
-    spam = spam.split()
-    spam_list = list(spam)
-    for i in range(len(spam_list)):
-        if spam_list(i) in spam_dict:
-            previous_val = spam_dict.get(i)
+    for i in range(len(spam)):
+        word = spam[i]
+        if spam[i] in SPAM_DICT:
+            previous_val = SPAM_DICT.get(word)
             new_val = previous_val + 1 
-            spam_dict[i] = new_val 
+            SPAM_DICT[i] = new_val 
         else:
-            spam_dict[spam_list(i)] = 0 #previously at 1 now at 0 idk
+            SPAM_DICT[spam(i)] = 0 #previously at 1 now at 0 idk
 
 
 def compare_to_words(message:str):
@@ -46,13 +46,13 @@ def compare_to_words(message:str):
     message_dict = {}
     for i in range(len(message_list)):
         word = message_list[i]
-        message_dict[word] = 0
+        message_dict[word] = int(0)
         if word in SPAM_DICT:
-            previous_val = message_dict.get(i)
-            new_val = previous_val + 1 #change 1 to the value in SPAM_DICT maybe
+            previous_val = message_dict.get(word)
+            new_val = previous_val + 10 #change 1 to the value in SPAM_DICT maybe
             message_dict[i] = new_val
         elif message_list[i] not in SPAM_DICT:
-            print('cool')
+            pass
     return message, message_dict
 
 
@@ -82,14 +82,25 @@ def get_bigrams(message:str):
         phrases_dict[phrase] = 0 
     return message, phrases_dict
 
+
+def spam_words(text):
+    test_string = text
+    message, message_dict = compare_to_words(text)
+    is_spam_or_not(message, message_dict)
+
+
+def spam_bigrams(text):
+    test_string = text
+    message, phrases = get_bigrams(test_string)
     
 
+def basic_test():
+    sentence_list = []
+    for i in range(len(sentence_list)):
+        spam_words(sentence_list[i])
 
 
-
-
-
-
+basic_test()
 
 #NOTES
 #is_spam_or_not function will take in a dictonary and string as its argument
