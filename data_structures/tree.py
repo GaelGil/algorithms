b27ca6aad880d9fc360abcf6da0fcb080c2beb7a
GaelGil <gaelgilbello27@gmail.com>
2021-11-02 18:14:28 -0700

    working on tree datastructure

--- data_structures/tree.py
+++ data_structures/tree.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, val=0, next=None)-> None:
+    def __init__(self, val=0, left=None, right=None)-> None:
         self.val = val
         self.left = None
         self.right = None
@@ -12,9 +12,39 @@ class Tree:
         pass
 
     def display(self):
+        print
         return
 
     def insert(self, data):
+        if self.root == None:
+            self.root = Node(data)
+        else:
+            self._insert(data, self.root)
         return
 
-    def delete(self, data)
+    def _insert(self, data, current_node):
+        if data < current_node.val:
+            if current_node.left == None:
+                current_node.left = Node(data)
+            else:
+                self._insert(data, current_node.left)
+        if data > current_node.val:
+            if current_node.right == None:
+                current_node.right = Node(data)
+            else:
+                self._insert(data, current_node.right)
+        return 0
+
+    def delete(self, data):
+        return
+
+
+tree = Tree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(18)
+tree.insert(3)
+
+
+tree.display()
+
