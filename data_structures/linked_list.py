588186e15ef49a0d31642169f964c38dba38da48
GaelGil <gaelgilbello27@gmail.com>
2022-04-09 18:48:09 -0700

    working on documentation for linked lists

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -3,49 +3,64 @@ from node import Node
 
 class LinkedList:
     """
-    A class used to represent a stack with nodes
+    A class used to represent a singly linked list.
 
     Attributes
     ----------
-    top : node
-        The node at the top of the stack
+    head: node
+        The head node of the linked list
+    size: int
+        The size of the linked list (how many items are in our list)
 
     Methods
     -------
-    push(self, data)
-        Insert node at the top of our stack
-    pop(self, data, current_node)
-        Remove the top node from the stack
-    get_top(self)
-        Return the top of the stack.
-    is_empty(self)
-        Check if the stack is empty.
+    append(self, data)
+        Insert node at the top of our stack.
+    add_infront(self, data)
+        Remove the top node from the stack.
     display(self)
         Get the sum of the tree.
+    get_head(self)
+        Return the head node of the linked list.
+    get_last(self)
+        Return the node at the end of the linked list.
+    includes(self, item)
+        Check if linked list contains an item.
+    get_index(self, index)
+        Get a node in the linked list by index.
+    get_size(self)
+        Return the size of the linked list.
+    sum(self)
+        Return the sum of the items in our linked list.
+    delete(self, item)
+        Delete and item if it is in our list
     """
     def __init__(self) -> None:
         self.head = Node()
         self.size = 0
 
     def append(self, data) -> None:
-        # TODO: finish docuementation
-        """Gets and prints the spreadsheet's header columns
+        """Add another node at the end of our linked list. If our head 
+        is empty we set our head to be equal to our new node. We also
+        increase the size of our linked list so we add that. Then we return
+        If our head is not empty we iterate all the way to the end of the list
+        until we get to the last node. We set the last nodes next value to be
+        our new node. We also increase our size by 1
+        If our head is empty the time complexcity to add is O(1).
+        If our head is not empty the time complexcity is O(n) because we have to
+        iterate the whole list to get to the end to add.
 
         Parameters
         ----------
-        file_loc : str
-            The file location of the spreadsheet
-        print_cols : bool, optional
-            A flag used to print the columns to the console (default is
-            False)
+        data: int
+            The value we are trying to store in our linked list
 
         Returns
         -------
-        list
-            a list of strings used that are the header columns
+        None
         """
         new_node = Node(data)
-        if self.head.val == None:
+        if not self.head.val:
             self.head = new_node
             self.size += 1
             return
@@ -57,21 +72,17 @@ class LinkedList:
         return
 
     def add_infront(self, data) -> None:
-        # TODO: finish docuementation
-        """Gets and prints the spreadsheet's header columns
+        """Add a node to the beggining of our linked list. 
+        The time complexcity of this is O(1) because we have access to the head.
 
         Parameters
         ----------
-        file_loc : str
-            The file location of the spreadsheet
-        print_cols : bool, optional
-            A flag used to print the columns to the console (default is
-            False)
+        data: int
+            The value we are trying to store in our linked list
 
         Returns
         -------
-        list
-            a list of strings used that are the header columns
+        None
         """
         new_node = Node(data)
         new_node.next = self.head
@@ -104,40 +115,31 @@ class LinkedList:
         return items
 
     def get_head(self):
-        # TODO: finish docuementation
-        """Gets and prints the spreadsheet's header columns
+        """Return the head node of the linked list
 
         Parameters
         ----------
-        file_loc : str
-            The file location of the spreadsheet
-        print_cols : bool, optional
-            A flag used to print the columns to the console (default is
-            False)
+        None
 
         Returns
         -------
-        list
-            a list of strings used that are the header columns
+        node
+            The head node.
         """
         return self.head
 
     def get_last(self):
-        # TODO: finish docuementation
-        """Gets and prints the spreadsheet's header columns
+        """Return the node at the end of the list. Iterate the list until we get to the end.
+        Then we can return that node.
 
         Parameters
         ----------
-        file_loc : str
-            The file location of the spreadsheet
-        print_cols : bool, optional
-            A flag used to print the columns to the console (default is
-            False)
+        None
 
         Returns
         -------
-        list
-            a list of strings used that are the header columns
+        node
+            The last node in our list.
         """
         current = self.head
         while current.next != None:
@@ -145,9 +147,11 @@ class LinkedList:
         return current
 
 
-    def get_item(self, item):
-        # TODO: finish docuementation
-        """Gets and prints the spreadsheet's header columns
+    def includes(self, item):
+        """Check if a certain item exists in our linked list. To do this we iterate
+        the whole list checking for the value.
+        The time complexcity of this is O(n) because in the worst case scenario we
+        have to iterate throug the whole list to find our value.
 
         Parameters
         ----------
@@ -200,21 +204,20 @@ class LinkedList:
 
 
     def sum(self):
-        # TODO: finish docuementation
-        """Gets and prints the spreadsheet's header columns
+        """Return the sum of the values in our linked list. To do this
+        we iterate the whole list adding the values of the nodes to a
+        counter. We then return the counter (sum_).
+        The time complexcity of this is O(n) because we have to iterate
+        the whole list to get the sum.
 
         Parameters
         ----------
-        file_loc : str
-            The file location of the spreadsheet
-        print_cols : bool, optional
-            A flag used to print the columns to the console (default is
-            False)
+        None
 
         Returns
         -------
-        list
-            a list of strings used that are the header columns
+        int
+            The sum of the values in our linked list.
         """
         sum_ = 0
         current = self.head
@@ -272,8 +275,8 @@ print(list.display())
 
 # print(list.get_last().val)
 
-print(list.get_item(2))
-print(list.get_item(6))
+print(list.includes(2))
+print(list.includes(6))
 
 print(list.get_index(5).val)
 print(list.get_index(7))
