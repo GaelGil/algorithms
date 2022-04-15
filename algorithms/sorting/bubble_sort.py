a2d22d7df090f91c6189f7b0a31870c76970e4f6
GaelGil <gaelgilbello27@gmail.com>
2022-04-14 18:02:02 -0700

    added small comment

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -3,7 +3,7 @@ nums = [[3, 10, 5, 12, 9, 20], [2, 8, 5, 3, 9, 4, 1]]
 
 def bubble_sort(nums):
     """Function to perform bubble sort. We do this by iterating through
-    the entire list. For each iteration we iterate again checking for
+    the entire list. For each iteration (i) we iterate again checking for
     pairs (j, j+1). If j is greater than j+1 then we swap it.
     The time complexcity of this is O(n^2) because we have to iterate
     through the list once and within that iterate through it again. 
