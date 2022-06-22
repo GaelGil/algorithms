8a2c21b9ed9feb3c800ba057fb5f8b032260fb51
GaelGil <gaelgilbello27@gmail.com>
2022-06-22 16:53:47 -0700

    added two functions to implement

---
+++ edlfyt/practice_three.py
@@ -0,0 +1,20 @@
+def compare_linear(nums_one:list, nums_two:list) -> int:
+    counter = 0
+    # for linear iterate through the list until you get a number 
+    # greater than
+    return counter
+
+
+
+def compare_binary_search(nums_one:list, nums_two:list) -> int:
+    counter = 0
+    return counter
+
+
+
+
+l1= [2,5,6,8,9,10]
+l2 = [8,9,10,2,5,6]
+print(compare_linear(l1, l2))
+
+print(compare_binary_search(l1, l2))
