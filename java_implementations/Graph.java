8ddeac3444f2d0ca205509b37f2061ce85013787
GaelGil <gaelgilbello27@gmail.com>
2022-08-23 09:32:57 -0700

    worked on graph implementation

--- java_implementations/Graph.java
+++ java_implementations/Graph.java
@@ -1,8 +1,41 @@
 import java.util.*;
 
 public class Graph {
+   private Dictionary graph = new Hashtable();
+ 
 
-   public static void main(String []args) {
-
+   Graph(char node){
+      this.graph.put(node, 0);
    }
+
+   Graph(char node, Dictionary neighbors){
+      this.graph.put(node, neighbors);
+   }
+
+   public void insert(char node, Dictionary neighbors){
+      if (node in graph){
+         System.out.println("already in graph");
+      }
+      else{
+         this.graph.put(node, neighbors);
+      }
+   }
+
+   public void bfs(){
+      // List<Node> queue = new ArrayList<>();
+      // queue.add(this.root);
+      // while (queue.size() > 0 ){
+      //    Node currentNode = queue.get(0);
+      //    queue.remove(0);
+      //    System.out.println(currentNode.val);
+      //    if (currentNode.left != null){
+      //       queue.add(currentNode.left);
+      //    }
+      //    if (currentNode.right != null){
+      //       queue.add(currentNode.right);
+      //    }
+      // }
+   }
+
 }
+
