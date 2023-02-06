da374411c4d1a87b470e258ed7c2a1160a011060
Gael <gaelgilbello27@gmail.com>
2023-02-05 18:07:01 -0800

    implementing merge sort

--- java_implementations/MergeSort.java
+++ java_implementations/MergeSort.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 public class MergeSort {
    public static void main(String []args) {
+      List<Integer> nums = new ArrayList<>();
+      nums.add(4);
+      nums.add(8);
+      nums.add(9);
+      nums.add(10);
+      nums.add(34);
+      nums.add(50);
+      nums.add(4);
+      nums.add(11);
+      nums.add(12);
+      nums.add(10);
+      nums.add(23);
+      nums.add(60);
+      nums.add(70);
+      nums.add(80);
 
-
-    numsOne.add(4);
-    numsOne.add(8);
-    numsOne.add(9);
-    numsOne.add(10);
-    numsOne.add(34);
-    numsOne.add(50);
-
-    numsTwo.add(4);
-    numsTwo.add(11);
-    numsTwo.add(12);
-    numsTwo.add(10);
-    numsTwo.add(23);
-    numsTwo.add(60);
-    numsTwo.add(70);
-    numsTwo.add(80);
-
+      System.out.println(nums);
 
    }
 }
